7cb3cfd79089b5bcd0c3ce71cea67b752490cf8f
gorenm49 <gorenityanand96@gmail.com>
2022-03-30 15:35:34 +0530

    [Nityanand]: implementation for reset the emp form done

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -90,6 +90,35 @@ function createAndUpdateStorage(employeePayrollData){
     localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
+const resetForm = () =>{
+    setValue ('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary',"40000");
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2022');
+
+}
+
+const unsetSelectedValues= (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue= (id,value) =>{
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue= (id,value) =>{
+    const element = document.querySelector(id);
+    element.value = value;
+}
 
 
 
